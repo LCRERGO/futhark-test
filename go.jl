55c23a05354c61c574056e0544b97ed0352b4145
Lucas Cruz dos Reis <lcr.ergo@gmail.com>
2020-11-10 23:55:06 -0300

    Starting the futhark dict

---
+++ go.jl
@@ -0,0 +1,29 @@
+futhark_mapping = 
+                Dict(
+                     "fehu"     => "\u16a0",
+                     "uruz"     => "\u16a2",
+                     "thurisaz" => "\u16a6",
+                     "raido"    => "\u16b1",
+                     "ansus"    => "\u16ab",
+                     "kenaz"    => "\u16b2",
+                     "gebo"     => "\u16b7",
+                     "wunjo"    => "\u16b9",
+
+                     "hagalaz"  => "\u16ba",
+                     "naudiz"   => "\u16be",
+                     "isaz"     => "\u16c1",
+                     "jera"     => "\u16c3",
+                     "eoh"      => "\u16c7",
+                     "peorth"   => "\u16c8",
+                     "algiz"    => "\u16c9",
+                     "sowilo"   => "\u16ca",
+
+                     "tewaz"    => "\u16cf",
+                     "berkana"  => "\u16d2",
+                     "ewaz"     => "\u16d6",
+                     "mannaz"   => "\u16d7",
+                     "laguz"    => "\u16da",
+                     "ingwaz"   => "\u16dc",
+                     "othal"    => "\u16df",
+                     "dagaz"    => "\u16de",
+                    )
